0e34a41259c462c4b9372e809cdfc05ff4e5ecb6
Dave2S <davidskacel@gmail.com>
2019-06-13 12:31:43 +0200

    In Terasology's PR#3680 I added a Tint Node before the output. I try bypass it from this module TODO: RenderGraphAPI will be just an interface probably? and the current RenderGraphAPI class will not be accessible

---
+++ src/main/java/org/terasology/rendering/TintNodeRemoval.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering;
+
+import org.terasology.context.Context;
+import org.terasology.engine.SimpleUri;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.registry.In;
+import org.terasology.rendering.dag.gsoc.NewNode;
+import org.terasology.rendering.dag.RenderGraph;
+import org.terasology.rendering.dag.api.RenderGraphAPI;
+
+// TODO NewAbstractNode to NewNode only!, no NewAbstractNode in here
+// TODO Decide on what must by marked as @API or whitelisted (like nodes and such...use renderGraphAPI to access main DAG)
+@RegisterSystem
+public class TintNodeRemoval extends BaseComponentSystem {
+
+    @In
+    Context context;
+
+    RenderGraphAPI renderGraphApi;
+
+    @Override
+    public void initialise() {
+        super.initialise();
+        renderGraphApi = context.get(RenderGraphAPI.class);
+
+        // TODO SHOULD THIS BE ACCESSIBLE? - NO
+        // RenderGraph rg = context.get(RenderGraph.class);
+
+        // TODO SHOULD THIS BE POSSIBLE from within a module ??
+        // NewNode finalPostProcessingNode = rg.findNode();
+        // NewNode tintNode = rg.findNode("engine:tintNode");
+        // NewNode outputToScreenNode = rg.findNode();
+
+        renderGraphApi.reconnectInputFboToOutput("engine:outputToScreenNode", 1, "engine:finalPostProcessingNode", 1);
+        // renderGraphApi.removeNode(new SimpleUri("engine:tintNode"));
+        // TODO disconnect and remove from renderGraph
+
+        // rg.addNode(new TintNode("aha",context));
+        // System.out.println(tintNode.toString());
+    }
+}
