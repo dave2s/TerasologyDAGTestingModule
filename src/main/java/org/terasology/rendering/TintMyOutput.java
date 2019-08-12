1985196849daeccad232fe5d3c68f4db4c50dc7d
Dave2S <davidskacel@gmail.com>
2019-08-12 19:44:11 +0200

    update TintMyOutput a little

--- src/main/java/org/terasology/rendering/TintMyOutput.java
+++ src/main/java/org/terasology/rendering/TintMyOutput.java
@@ -15,39 +15,43 @@
  */
 package org.terasology.rendering;
 
+import org.terasology.context.Context;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.rendering.dag.gsoc.ModuleRenderingSystem;
+import org.terasology.rendering.dag.gsoc.ModuleRendering;
 import org.terasology.rendering.dag.gsoc.NewNode;
 import org.terasology.rendering.dag.nodes.TintNode;
 
 // TODO NewAbstractNode to NewNode only!, no NewAbstractNode in here
 // TODO Decide on what must by marked as @API or whitelisted (like nodes and such...use renderGraphAPI to access main DAG)
 @RegisterSystem
-public class TintMyOutput extends ModuleRenderingSystem {
+public class TintMyOutput extends ModuleRendering {
 
-    @Override
+    public TintMyOutput(Context context) {
+        super(context);
+    }
+
     public void initialise() {
         super.initialise();
         // TODO hide this..reflection?
-        super.setProvidingModule(this.getClass());
+           // super.setProvidingModule(this.getClass());
         moduleTintOutput();
     }
 
     private void moduleTintOutput() {
         // Create a new tintNode
         renderGraph.addShader("tint", providingModule);
-        NewNode tintNode = new TintNode("tintNode", context);
+        NewNode tintNode = new TintNode("tintNode", providingModule, context);
         // TODO connectEShader(distinguish between engine based and module based);
 
         // TODO nice to have, many possibilities of autonomous insertion
         // renderDagApi.insertBefore(tintNode, "engine:outputToScreenNode");
 
-        renderGraph.disconnectOutputFbo("engine:finalPostProcessingNode", 1);
-        renderGraph.reconnectInputFboToOutput(tintNode, 1, "engine:finalPostProcessingNode", 1);
+        renderGraph.disconnectOutputFboConnection("engine:finalPostProcessingNode", 1);
+        renderGraph.reconnectInputFboToOutput("engine:finalPostProcessingNode", 1, tintNode, 1, true);
         renderGraph.addNode(tintNode);
 
         NewNode outputToScreenNode = renderGraph.findNode("engine:outputToScreenNode");
-        renderGraph.reconnectInputFboToOutput(outputToScreenNode, 1, "DagTestingModule:tintNode", 1);
-        outputToScreenNode.resetDesiredStateChanges();
+        renderGraph.reconnectInputFboToOutput("DagTestingModule:tintNode", 1, outputToScreenNode, 1, true);
+        //outputToScreenNode.resetDesiredStateChanges();
     }
 }
